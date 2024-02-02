1ada7194c39c48b07f2b12844259e8ec912c9232
Mochammad Gamal <mogams1811@gmail.com>
2024-02-02 15:08:31 +0700

    chore: :hammer: progress implementasi constructor class

--- app.js
+++ app.js
@@ -5,21 +5,21 @@
 //     return `rgb(${r}, ${g}, ${b})`;
 // };
 // ! simple concept of factory function
-function convertColor(r, g, b) {
-    const color = {};
-    color.r = r;
-    color.g = g;
-    color.b = b;
-    color.rgb = function () {
-        const {r, g, b} = this;
-        return `rgb(${r}, ${b}, ${b})`;
-    };
-    color.hex = function () {
-        const {r, g, b} = this;
-        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-    };
-    return color;
-};
+// function convertColor(r, g, b) {
+//     const color = {};
+//     color.r = r;
+//     color.g = g;
+//     color.b = b;
+//     color.rgb = function () {
+//         const {r, g, b} = this;
+//         return `rgb(${r}, ${b}, ${b})`;
+//     };
+//     color.hex = function () {
+//         const {r, g, b} = this;
+//         return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+//     };
+//     return color;
+// };
 // ! simple copcept of constructor
 function Color(r, g, b) {
     this.r = r;
